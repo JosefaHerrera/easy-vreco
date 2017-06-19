223219077daf0f7424e16fe071dfb4e588ed567e
JosefaHerrera <josefa@linuxero.cl>
2017-06-19 17:17:05 -0400

    arreglando

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ function initMap() {
         streetViewControl: false
     });
 
-    function buscar() {
+
+        function buscar() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
         }
@@ -34,4 +35,83 @@ function initMap() {
     var funcionError = function(error) {
         alert('Tenemos un problema con encontrar tu ubicación');
     }
-}
+                            /*RUTA*/
+
+
+    /* AUTOCOMPRETADO INPUT */
+    //input1
+    var inputOrigen = (document.getElementById('origen'));
+    var autocomplete = new google.maps.places.Autocomplete(inputOrigen);
+        autocomplete.bindTo('bounds', map);
+    //input2
+    var inputDestino = (document.getElementById('destino'));
+    var autocomplete = new google.maps.places.Autocomplete(inputDestino);
+        autocomplete.bindTo('bounds', map);
+
+/*DirectionsService:
+Puedes calcular indicaciones (usando varios métodos de transporte) con el objeto DirectionsService. 
+Este objeto se comunica con el servicio de indicaciones de la Google Maps API,
+ el cual recibe solicitudes de indicaciones y devuelve resultados computados.
+Puedes administrar estos resultados de indicaciones por ti mismo o usar el objeto DirectionsRenderer para representarlos.*/
+
+    /*INDICACIONES * Servicio de indicaciones*/
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+
+    document.getElementById('origen').addEventListener('change', onChangeHandler);
+    document.getElementById('destino').addEventListener('change', onChangeHandler);
+
+
+    function lineaRuta(directionsService, directionsDisplay) {
+        directionsService.route({
+            origin: document.getElementById('origen').value,
+            destination: document.getElementById('destino').value,
+            travelMode: 'DRIVING'
+            },
+        function(response, status) {
+            if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+            } else {
+                window.alert('Ruta no disponible ' + status);
+            }
+        });
+    }
+    directionsDisplay.setMap(map);
+        //onChangeHandler = Agregar una propiedad de seguimiento a una definición de lenguaje específico de dominio
+        var onChangeHandler = function(){
+            //Servicio de indicaciones
+        lineaRuta(directionsService, directionsDisplay);
+    };  
+        
+    document.getElementById("ruta").addEventListener("click",onChangeHandler); 
+};
+
+
+
+/*
+Modos de viaje
+
+Al calcular indicaciones, debes especificar el modo de transporte que se usará. Actualmente, 
+se admiten los siguientes modos de viaje:
+
+DRIVING (predeterminado) establece indicaciones de manejo estándar por la red de carreteras.
+BICYCLING solicita indicaciones para el traslado en bicicleta por ciclovías y calles preferidas.
+TRANSIT solicita indicaciones por rutas de transporte público.
+WALKING solicita indicaciones de traslado a pie por sendas peatonales y veredas.
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
